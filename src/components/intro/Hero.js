758d50230af335af650601263cd69398436924d7
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-15 20:01:39 +0530

    added hero component with style

---
+++ src/components/intro/Hero.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BgIcon from "../../images/bg-intro-desktop.svg";
+import "./hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="left">
+        <h1 className="title">Next generation digital banking</h1>
+        <p className="sub-title">
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button className="request-btn">request invite</button>
+      </div>
+      <div className="right">
+        <img src={BgIcon} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
