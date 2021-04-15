f0ab3528d46dcf02d966d0b561391c4044f789f0
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-15 20:02:01 +0530

    moved root variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/header/Navbar";
+import Hero from "./components/intro/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
