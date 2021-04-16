dcefa3cd7c3dc9cc7b50467f83b32d3742aaf8b5
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-16 19:17:51 +0530

    feature list json

---
+++ src/components/intro/feature.js
@@ -0,0 +1,30 @@
+export const feature = [
+  {
+    id: 1,
+    logo: "../../images/icon-online.svg",
+    text: "Online Banking",
+    description:
+      "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    id: 2,
+    logo: "../../images/icon-budgeting.svg",
+    text: "Simple Budgeting",
+    description:
+      "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+  },
+  {
+    id: 3,
+    logo: "../../images/icon-onboarding.svg",
+    text: "Fast Onboarding",
+    description:
+      "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+  {
+    id: 4,
+    logo: "../../images/icon-api.svg",
+    text: "Open API",
+    description:
+      "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+  },
+];
