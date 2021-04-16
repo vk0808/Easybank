5ec68d7e104d0786d236cafba912b2cf6d96ad73
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-16 19:19:00 +0530

    included subsection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/header/Navbar";
 import Hero from "./components/intro/Hero";
+import SubSection from "./components/intro/SubSection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <SubSection />
     </>
   );
 }
