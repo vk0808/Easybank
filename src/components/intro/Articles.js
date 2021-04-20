91afcd5d8e85adf1851011bc92300a3842c70374
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-20 21:34:05 +0530

    add common style class

--- src/components/intro/Articles.js
+++ src/components/intro/Articles.js
@@ -7,24 +7,28 @@ import img_4 from "../../images/image-confetti.jpg";
 
 function Articles() {
   return (
-    <section className="article">
+    <section className="feature article">
       <h1 className="head">Latest Articles</h1>
-      {article.map((post) => {
-        const { id, img, author, title, description } = post;
-        return (
-          <div key={id} className="post">
-            <figure>
-              {id === 1 && <img src={img_1} alt="" />}
-              {id === 2 && <img src={img_2} alt="" />}
-              {id === 3 && <img src={img_3} alt="" />}
-              {id === 4 && <img src={img_4} alt="" />}
-              <figcaption>By {author}</figcaption>
-            </figure>
-            <h2>{title}</h2>
-            <p>{description}</p>
-          </div>
-        );
-      })}
+      <div className="posts feature-container">
+        {article.map((post) => {
+          const { id, img, author, title, description } = post;
+          return (
+            <div key={id} className="post">
+              <figure>
+                {id === 1 && <img src={img_1} alt="" />}
+                {id === 2 && <img src={img_2} alt="" />}
+                {id === 3 && <img src={img_3} alt="" />}
+                {id === 4 && <img src={img_4} alt="" />}
+              </figure>
+              <div className="inner-detail">
+                <figcaption>By {author}</figcaption>
+                <h2>{title}</h2>
+                <p>{description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
