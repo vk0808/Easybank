509af5637813b7b47c6af3bc7e257eca4f0d10fd
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-23 21:02:19 +0530

    created footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { links } from "./dataFooter";
+import { ReactComponent as Facebook } from "../../images/icon-facebook.svg";
+import { ReactComponent as Youtube } from "../../images/icon-youtube.svg";
+import { ReactComponent as Twitter } from "../../images/icon-twitter.svg";
+import { ReactComponent as Pinterest } from "../../images/icon-pinterest.svg";
+import { ReactComponent as Instagram } from "../../images/icon-instagram.svg";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-left">
+        <div className="a">
+          <div className="footer-logo">
+            <Logo />
+          </div>
+          <div className="social-links">
+            <a href="www.facebook.com" target="_blank">
+              <Facebook />
+            </a>
+            <a href="www.youtube.com" target="_blank">
+              <Youtube />
+            </a>
+            <a href="www.twitter.com" target="_blank">
+              <Twitter />
+            </a>
+            <a href="www.pinterest.com" target="_blank">
+              <Pinterest />
+            </a>
+            <a href="www.instagram.com" target="_blank">
+              <Instagram />
+            </a>
+          </div>
+        </div>
+        <div className="b">
+          <div className="links">
+            {links.map((link) => {
+              const { id, text, url } = link;
+              return (
+                <p key={id}>
+                  <a href={url}>{text}</a>
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="footer-right">
+        <div className="btn-div">
+          <button className="request-btn">request invite</button>
+        </div>
+        <div className="copyright"> &copy; Easybank. All Rights Reserved</div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
