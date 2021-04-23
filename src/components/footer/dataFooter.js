7cc3102daf2e7627cb6cfcc6783a2bef7817aa90
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-23 21:03:32 +0530

    created footer links json

---
+++ src/components/footer/dataFooter.js
@@ -0,0 +1,32 @@
+export const links = [
+  {
+    id: 1,
+    url: "/about",
+    text: "about us",
+  },
+  {
+    id: 2,
+    url: "/career",
+    text: "career",
+  },
+  {
+    id: 3,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 4,
+    url: "/support",
+    text: "support",
+  },
+  {
+    id: 5,
+    url: "/blog",
+    text: "blog",
+  },
+  {
+    id: 6,
+    url: "/privacy_policy",
+    text: "privacy policy",
+  },
+];
