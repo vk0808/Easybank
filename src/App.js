c6efd868813076bfcbfb0ef2db623223d975b421
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-24 17:11:50 +0530

    included router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/header/Navbar";
+import Footer from "./components/footer/Footer";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import Career from "./pages/Career";
+import SingleBlog from "./pages/SingleBlog";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/career">
+          <Career />
+        </Route>
+        <Route path="/blog/:id" children={<SingleBlog />}></Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
