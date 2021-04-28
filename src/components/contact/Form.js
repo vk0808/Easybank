f1288209c97f41bc506caec9622eb64f729f861d
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-28 20:20:40 +0530

    added contact-right

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -49,6 +49,50 @@ function ContactForm() {
             </div>
           </form>
         </div>
+        <div className="contact-right">
+          <h3>Get in touch with us</h3>
+          <div className="contact-info">
+            <div className="card">
+              <FiMail />
+              <div>
+                <h4>Email</h4>
+                <p>contact@easybank.in</p>
+              </div>
+            </div>
+            <div className="card">
+              <FiPhone />
+              <div>
+                <h4>Phone</h4>
+                <p>+91 1234567890</p>
+              </div>
+            </div>
+            <div className="card">
+              <FiMapPin />
+              <div>
+                <h4>Address</h4>
+                <p>Whitefield, Bangalore</p>
+                <p>India</p>
+              </div>
+            </div>
+          </div>
+          <div className="social-links">
+            <a href="www.facebook.com" target="_blank">
+              <Facebook />
+            </a>
+            <a href="www.youtube.com" target="_blank">
+              <Youtube />
+            </a>
+            <a href="www.twitter.com" target="_blank">
+              <Twitter />
+            </a>
+            <a href="www.pinterest.com" target="_blank">
+              <Pinterest />
+            </a>
+            <a href="www.instagram.com" target="_blank">
+              <Instagram />
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
