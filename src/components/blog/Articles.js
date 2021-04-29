1428ec96aa77e498f25429c38bb96b098390f845
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-29 18:44:27 +0530

    recreated and modified components

---
+++ src/components/blog/Articles.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { posts } from "./blogPost";
+import img_1 from "../../images/image-currency.jpg";
+import img_2 from "../../images/image-restaurant.jpg";
+import img_3 from "../../images/image-plane.jpg";
+import img_4 from "../../images/image-confetti.jpg";
+
+function Articles({ query }) {
+  const filterPosts = (posts, query) => {
+    if (!query) {
+      return posts;
+    }
+
+    return posts.filter((post) => {
+      const postName = post.title.toLowerCase();
+      return postName.includes(query);
+    });
+  };
+  const filteredPosts = filterPosts(posts, query);
+
+  return (
+    <section className="feature article">
+      <div className="posts feature-container">
+        {filteredPosts.length !== 0 ? (
+          filteredPosts.map((post) => {
+            const { id, author, title, description } = post;
+            return (
+              <div key={id} className="post">
+                <figure>
+                  {id === 1 && <img src={img_1} alt="" />}
+                  {id === 2 && <img src={img_2} alt="" />}
+                  {id === 3 && <img src={img_3} alt="" />}
+                  {id === 4 && <img src={img_4} alt="" />}
+                </figure>
+                <div className="inner-detail">
+                  <Link className="article-title" to={`/blog/${id}`}>
+                    <h2>{title}</h2>
+                  </Link>
+                  <figcaption>By {author}</figcaption>
+                  <p>{description}</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <div className="no-post">
+            <p>Can't find the post with {query}</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Articles;
---
+++ src/components/blog/blogPost.js
@@ -0,0 +1,34 @@
+export const posts = [
+  {
+    id: 1,
+    img: "../../images/image-currency.jpg",
+    author: "Claire Robinson",
+    title: "Receive money in any currency with no fees",
+    description:
+      "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+  },
+  {
+    id: 2,
+    img: "../../images/image-restaurant.jpg",
+    author: "Wilson Hutton",
+    title: "Treat yourself without worrying about money",
+    description:
+      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you ...",
+  },
+  {
+    id: 3,
+    img: "../../images/image-plane.jpg",
+    author: "Wilson Hutton",
+    title: "Take your Easybank card wherever you go",
+    description:
+      "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you ...",
+  },
+  {
+    id: 4,
+    img: "../../images/image-confetti.jpg",
+    author: "Claire Robinson",
+    title: "Our invite-only Beta accounts are now live!",
+    description:
+      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+  },
+];
