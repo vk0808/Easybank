a79112f829e40ec7c74dbb87798d213b11a3c54c
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-01 21:38:05 +0530

    cleared unnecessary code

--- src/components/blog/Articles.js
+++ src/components/blog/Articles.js
@@ -3,10 +3,6 @@ import { Link } from "react-router-dom";
 import LinesEllipsis from "react-lines-ellipsis";
 
 import { posts } from "./blogPost";
-import img_1 from "../../images/image-currency.jpg";
-import img_2 from "../../images/image-restaurant.jpg";
-import img_3 from "../../images/image-plane.jpg";
-import img_4 from "../../images/image-confetti.jpg";
 
 function Articles({ query }) {
   const filterPosts = (posts, query) => {
@@ -27,17 +23,14 @@ function Articles({ query }) {
       <div className="posts feature-container">
         {filteredPosts.length !== 0 ? (
           filteredPosts.map((post) => {
-            const { id, author, title, description } = post;
+            const { id, img, author, title, description } = post;
             return (
               <div key={id} className="post">
                 <figure>
-                  {id === 1 && <img src={img_1} alt="" />}
-                  {id === 2 && <img src={img_2} alt="" />}
-                  {id === 3 && <img src={img_3} alt="" />}
-                  {id === 4 && <img src={img_4} alt="" />}
+                  <img src={`./images/${img}`} alt={author} />
                 </figure>
                 <div className="inner-detail">
-                  <Link className="article-title" to={`/blog/${id}`}>
+                  <Link className="article-title" to={`/article/${id}`}>
                     <h2>{title}</h2>
                   </Link>
                   <figcaption>By {author}</figcaption>
--- src/components/intro/Articles.js
+++ src/components/intro/Articles.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { article } from "./article";
-import img_1 from "../../images/image-currency.jpg";
-import img_2 from "../../images/image-restaurant.jpg";
-import img_3 from "../../images/image-plane.jpg";
-import img_4 from "../../images/image-confetti.jpg";
 
 function Articles() {
   return (
@@ -13,14 +9,11 @@ function Articles() {
       <h1 className="head">Latest Articles</h1>
       <div className="posts feature-container">
         {article.map((post) => {
-          const { id, author, title, description } = post;
+          const { id, img, author, title, description } = post;
           return (
             <div key={id} className="post">
               <figure>
-                {id === 1 && <img src={img_1} alt="" />}
-                {id === 2 && <img src={img_2} alt="" />}
-                {id === 3 && <img src={img_3} alt="" />}
-                {id === 4 && <img src={img_4} alt="" />}
+                <img src={img} alt="" />
               </figure>
               <div className="inner-detail">
                 <figcaption>By {author}</figcaption>
