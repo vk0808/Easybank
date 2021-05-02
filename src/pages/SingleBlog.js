6567f00d8779d0f61e509ce278730853b2dba0dd
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-02 20:17:13 +0530

    changed class and added figcaption

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -1,31 +1,34 @@
 import React from "react";
 import { BiUserCircle } from "react-icons/bi";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import { posts } from "../components/blog/blogPost";
 
 function SingleBlog() {
   const { id } = useParams();
   const specificPost = posts.filter((post) => JSON.stringify(post.id) === id);
-  const count = 0;
 
   return (
     <>
       {specificPost.map((post) => {
-        const { id, img, title, description, author, date } = post;
+        const { id, img, photo, title, description, author, date } = post;
         return (
           <div className="specific-post" key={id}>
             <h1 className="title">{title}</h1>
-            <p>
-              <BiUserCircle /> {author}
-            </p>
-            <p>{date}</p>
+            <div className="author-details">
+              <p>
+                <span>by </span>
+                <BiUserCircle /> {author}
+              </p>
+              <p className="date">{date}</p>
+            </div>
             <figure>
               <img src={`../images/${img}`} alt="" />
+              <figcaption>Photo by {photo}</figcaption>
             </figure>
             <p className="description">
               {description.split("\n\n").map((paragraph) => (
-                <p className={`para-${count}`}>
+                <p className="para">
                   {paragraph
                     .split("\n")
                     .reduce((total, line) => [total, <br />, line])}
