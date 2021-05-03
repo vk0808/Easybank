98115ab0d1c93c750a4edf11c13e1334e65c4425
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-03 23:13:07 +0530

    included more content

--- src/components/intro/article.js
+++ src/components/intro/article.js
@@ -5,7 +5,7 @@ export const article = [
     author: "Claire Robinson",
     title: "Receive money in any currency with no fees",
     description:
-      "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+      "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const article = [
     author: "Wilson Hutton",
     title: "Treat yourself without worrying about money",
     description:
-      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you ...",
+      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const article = [
     author: "Wilson Hutton",
     title: "Take your Easybank card wherever you go",
     description:
-      "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you ...",
+      "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const article = [
     author: "Claire Robinson",
     title: "Our invite-only Beta accounts are now live!",
     description:
-      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
 ];
