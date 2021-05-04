ef4f89e5b1cd99eba0ebfc8b7c718ec784d64a7f
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-04 14:53:59 +0530

    reordered post sequence

--- src/components/intro/article.js
+++ src/components/intro/article.js
@@ -1,22 +1,14 @@
 export const article = [
   {
     id: 1,
-    img: "./images/image-currency.jpg",
+    img: "./images/image-confetti.jpg",
     author: "Claire Robinson",
-    title: "Receive money in any currency with no fees",
+    title: "Our invite-only Beta accounts are now live!",
     description:
-      "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
+      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
   {
     id: 2,
-    img: "./images/image-restaurant.jpg",
-    author: "Wilson Hutton",
-    title: "Treat yourself without worrying about money",
-    description:
-      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
-  },
-  {
-    id: 3,
     img: "./images/image-plane.jpg",
     author: "Wilson Hutton",
     title: "Take your Easybank card wherever you go",
@@ -24,11 +16,19 @@ export const article = [
       "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
   {
-    id: 4,
-    img: "./images/image-confetti.jpg",
-    author: "Claire Robinson",
-    title: "Our invite-only Beta accounts are now live!",
+    id: 3,
+    img: "./images/image-restaurant.jpg",
+    author: "Wilson Hutton",
+    title: "Treat yourself without worrying about money",
     description:
-      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
+      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
+  },
+  {
+    id: 4,
+    img: "./images/image-currency.jpg",
+    author: "Claire Robinson",
+    title: "Receive money in any currency with no fees",
+    description:
+      "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eveniet expedita. Quo aperiam ipsam numquam adipisci repellendus? Deserunt, tempore deleniti?",
   },
 ];
--- src/components/singleBlog/blogPost.js
+++ src/components/singleBlog/blogPost.js
@@ -1,45 +1,14 @@
 export const posts = [
   {
     id: 1,
-    img: "image-currency.jpg",
-    photo: "Chris",
+    img: "image-confetti.jpg",
+    photo: "Alex",
     author: "Claire Robinson",
     date: "3 March 2021",
-    title: "Receive money in any currency with no fees",
+    title: "Our invite-only Beta accounts are now live!",
     description: `
       <div class="para">
-        <p>The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single.</p> 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <h2 class="para-header">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <p class="para-fact">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere, accusamus aperiam excepturi.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <p class="para-statement">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quis, sed maxime iure eligendi reiciendis omnis commodi fuga earum optio.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <p class="para-quote">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sit facilis perspiciatis reprehenderit quo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <div class="para-group">
-        <p class="para-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, alias.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        </div>
-        <div class="para-group">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-        <p class="para-right">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, alias.</p>
-        </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
-      </div>
-      `,
-  },
-  {
-    id: 2,
-    img: "image-restaurant.jpg",
-    photo: "James",
-    author: "Wilson Hutton",
-    date: "4 March 2021",
-    title: "Treat yourself without worrying about money",
-    description: `
-      <div class="para">
-        <p>Our simple budgeting feature allows you to separate out your spending and set realistic limits each month.</p> 
+        <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site.</p> 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
         <h2 class="para-header">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
@@ -62,11 +31,11 @@ export const posts = [
       `,
   },
   {
-    id: 3,
+    id: 2,
     img: "image-plane.jpg",
     photo: "John",
     author: "Wilson Hutton",
-    date: "5 March 2021",
+    date: "4 March 2021",
     title: "Take your Easybank card wherever you go",
     description: `
       <div class="para">
@@ -93,15 +62,15 @@ export const posts = [
       `,
   },
   {
-    id: 4,
-    img: "image-confetti.jpg",
-    photo: "Alex",
-    author: "Claire Robinson",
-    date: "6 March 2021",
-    title: "Our invite-only Beta accounts are now live!",
+    id: 3,
+    img: "image-restaurant.jpg",
+    photo: "James",
+    author: "Wilson Hutton",
+    date: "5 March 2021",
+    title: "Treat yourself without worrying about money",
     description: `
       <div class="para">
-        <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site.</p> 
+        <p>Our simple budgeting feature allows you to separate out your spending and set realistic limits each month.</p> 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
         <h2 class="para-header">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
@@ -121,6 +90,37 @@ export const posts = [
         </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
       </div>  
+      `,
+  },
+  {
+    id: 4,
+    img: "image-currency.jpg",
+    photo: "Chris",
+    author: "Claire Robinson",
+    date: "6 March 2021",
+    title: "Receive money in any currency with no fees",
+    description: `
+      <div class="para">
+        <p>The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single.</p> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <h2 class="para-header">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <p class="para-fact">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti facere, accusamus aperiam excepturi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <p class="para-statement">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta quis, sed maxime iure eligendi reiciendis omnis commodi fuga earum optio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <p class="para-quote">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sit facilis perspiciatis reprehenderit quo.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <div class="para-group">
+        <p class="para-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, alias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        </div>
+        <div class="para-group">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+        <p class="para-right">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, alias.</p>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptate voluptas reiciendis! Eaque, dolorum debitis fuga exercitationem qui impedit eius earum autem deleniti. Molestiae eveniet doloribus praesentium quasi nam totam numquam voluptates suscipit, beatae deleniti, perspiciatis, earum quae architecto debitis.</p>
+      </div>
       `,
   },
 ];
