cdf17f3b8aa1918e854a729d8f24b291866a9a8f
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-06 19:39:15 +0530

    added sample job data

---
+++ src/components/careers/jobs.js
@@ -0,0 +1,452 @@
+export const jobs = [
+  {
+    id: "pd010451",
+    department: "Product Development",
+    country: "india",
+    location: "bangalore",
+    role: "Application Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd020431",
+    department: "Product Development",
+    country: "japan",
+    location: "tokyo",
+    role: "full stack Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd010452",
+    department: "Product Development",
+    country: "india",
+    location: "bangalore",
+    role: "jr. frontend Developer",
+    type: "internship",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd030444",
+    department: "Product Development",
+    country: "south korea",
+    location: "seoul",
+    role: "sr backend Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd040412",
+    department: "Product Development",
+    country: "malaysia",
+    location: "kuala lumpur",
+    role: "React Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd050472",
+    department: "Product Development",
+    country: "usa",
+    location: "new york city",
+    role: "Application Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd050471",
+    department: "Product Development",
+    country: "usa",
+    location: "new york city",
+    role: "full stack Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd060461",
+    department: "Product Development",
+    country: "canada",
+    location: "toronto",
+    role: "sr frontend Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd070452",
+    department: "Product Development",
+    country: "germany",
+    location: "berlin",
+    role: "sr backend Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd080412",
+    department: "Product Development",
+    country: "brazil",
+    location: "brasilia",
+    role: "React Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd080413",
+    department: "Product Development",
+    country: "brazil",
+    location: "brasilia",
+    role: "Application Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "pd080414",
+    department: "Product Development",
+    country: "brazil",
+    location: "brasilia",
+    role: "full stack Developer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>Analyze, design develop, troubleshoot and debug software programs for commercial or end user applications. Writes code, completes programming and performs testing and debugging of applications.
+</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "it010512",
+    department: "Information Technology",
+    country: "india",
+    location: "bangalore",
+    role: "IT Support Engineer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "it080512",
+    department: "Information Technology",
+    country: "brazil",
+    location: "brasilia",
+    role: "Cloud Integration Ops Engineer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "it060512",
+    department: "Information Technology",
+    country: "canada",
+    location: "toronto",
+    role: "Cloud Integration Ops Engineer",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Fintech product development experience is preferred</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "fa050662",
+    department: "Finance & Accounting",
+    country: "usa",
+    location: "new york city",
+    role: "Accountant",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "fa040632",
+    department: "Finance & Accounting",
+    country: "malaysia",
+    location: "Kuala Lumpur",
+    role: "Accountant",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "fa030627",
+    department: "Finance & Accounting",
+    country: "South Korea",
+    location: "seoul",
+    role: "Auditor",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+    </ul>
+    `,
+  },
+  {
+    id: "boa050712",
+    department: "Business Operations/Analysis",
+    country: "usa",
+    location: "new york city",
+    role: "Business Analyst",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+      <li>Analyze, design, develop, troubleshoot and debug solutions</li>
+      <li>4 years of experience in BOA or related field</li>
+    </ul>
+    `,
+  },
+  {
+    id: "boa070706",
+    department: "Business Operations/Analysis",
+    country: "Germany",
+    location: "Berlin",
+    role: "Financial Analyst",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+      <li>Analyze, design, develop, troubleshoot and debug solutions</li>
+      <li>4 years of experience in BOA or related field</li>
+    </ul>
+    `,
+  },
+  {
+    id: "boa010701",
+    department: "Business Operations/Analysis",
+    country: "india",
+    location: "bangalore",
+    role: "Business Analyst",
+    type: "full time",
+    posting_date: "",
+    appy_date: "",
+    job_shift: "day",
+    description: ``,
+    requirement: `
+    <ul>
+      <li>Ability to collaborate and work effectively in a team</li>
+      <li>BS or MS degree or equivalent experience relevant to functional area</li>
+      <li>Duties and tasks are varied and complex needing independent judgment</li>
+      <li>Excellent verbal and written communication skills</li>
+      <li>Analyze, design, develop, troubleshoot and debug solutions</li>
+      <li>4 years of experience in BOA or related field</li>
+    </ul>
+    `,
+  },
+];
