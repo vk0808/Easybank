01e31461881936b04ac433753a2435e1e7ca1eb3
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-07 17:24:05 +0530

    add benefits json

---
+++ src/components/careers/benefits.js
@@ -0,0 +1,32 @@
+export const benefits = [
+  {
+    id: "1",
+    img: "images/career/book.svg",
+    content: "Learning and development",
+  },
+  {
+    id: "2",
+    img: "images/career/calendar-outline.svg",
+    content: "Paid volunteer time",
+  },
+  {
+    id: "3",
+    img: "images/career/fitness.svg",
+    content: "Subsciption to fitness classes",
+  },
+  {
+    id: "4",
+    img: "images/career/health-outline.svg",
+    content: "Comprehensive health plans",
+  },
+  {
+    id: "5",
+    img: "images/career/healthy-meal-outline.svg",
+    content: "Healthy food and snacks",
+  },
+  {
+    id: "6",
+    img: "images/career/scooter.svg",
+    content: "Commuter Benefit Program ",
+  },
+];
