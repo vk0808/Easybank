64b48109bf61c53fbcbde848145851745299bbfe
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-08 17:43:27 +0530

    added Roles component

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import Careers from "../components/careers/Careers";
+import Roles from "../components/careers/Roles";
 
 function Career() {
-  return <Careers />;
+  return (
+    <>
+      <Careers />
+      <Roles />
+    </>
+  );
 }
 
 export default Career;
