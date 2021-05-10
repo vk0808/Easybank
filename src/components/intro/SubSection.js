00c007ba4e73152ce68396b65396f9b510bc8ec5
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-10 09:21:22 +0530

    change logo path

--- src/components/intro/SubSection.js
+++ src/components/intro/SubSection.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { feature } from "./feature";
-import logo_1 from "../../images/icon-online.svg";
-import logo_2 from "../../images/icon-budgeting.svg";
-import logo_3 from "../../images/icon-onboarding.svg";
-import logo_4 from "../../images/icon-api.svg";
 
 function SubSection() {
   return (
@@ -17,14 +13,11 @@ function SubSection() {
       </div>
       <div className="feature-container">
         {feature.map((item) => {
-          const { id, text, description } = item;
+          const { id, logo, text, description } = item;
           return (
             <div key={id} className="feature-list">
               <div className="feature-logo">
-                {id === 1 && <img src={logo_1} alt="" />}
-                {id === 2 && <img src={logo_2} alt="" />}
-                {id === 3 && <img src={logo_3} alt="" />}
-                {id === 4 && <img src={logo_4} alt="" />}
+                <img src={`./images/intro/${logo}`} alt="" />
               </div>
               <h1>{text}</h1>
               <p>{description}</p>
