30d6fe63ab6d649606839e6e95115cc7b66a1287
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-10 13:49:09 +0530

    updated go back btn

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { ReactComponent as NotFound } from "../images/page-not-found-outline.svg";
 import "../css/App.css";
@@ -7,6 +8,9 @@ function Error() {
   return (
     <div className="not-found">
       <NotFound />
+      <Link className="request-btn" to="/">
+        Go back to Home
+      </Link>
     </div>
   );
 }
