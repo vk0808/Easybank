f9f53dfd0f3803c9027d487c60123d3fd5c1ec56
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-10 14:42:27 +0530

    created separate pages

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../components/header/Navbar";
+import Footer from "../components/footer/Footer";
+import Privacy from "../components/privacy/Privacy";
+
+function PrivacyPolicy() {
+  return (
+    <>
+      <Navbar />
+      <Privacy />
+      <Footer />
+    </>
+  );
+}
+
+export default PrivacyPolicy;
---
+++ src/pages/TermsCondition.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../components/header/Navbar";
+import Footer from "../components/footer/Footer";
+import Terms from "../components/privacy/Terms";
+
+function TermsCondition() {
+  return (
+    <>
+      <Navbar />
+      <Terms />
+      <Footer />
+    </>
+  );
+}
+
+export default TermsCondition;
