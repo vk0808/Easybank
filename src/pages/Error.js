f96d1aa2dad1fae0f4330f582c83106bd3429849
VINOD KUMAR R <ravivinod21@gmail.com>
2021-05-10 13:28:13 +0530

    added 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { ReactComponent as NotFound } from "../images/page-not-found-outline.svg";
+
 function Error() {
   return (
-    <div>
-      <p>404 no found</p>
+    <div className="not-found">
+      <NotFound />
     </div>
   );
 }
